726d42617dd3681998a246186416ac12b18189fe
ariankharazmi <100003892+ariankharazmi@users.noreply.github.com>
2026-03-17 01:59:45 -0400

    Week 2 - Jawbreak App

---
+++ Week 2/ARFaceScannerView.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+import ARKit
+import SceneKit
+
+struct ARFaceScannerView: UIViewRepresentable {
+    
+}
+
---
+++ Week 2/JawbreakTest1App.swift
@@ -0,0 +1,17 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+Contains the application's delegate.
+*/
+
+import SwiftUI
+
+@main
+struct JawbreakTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
